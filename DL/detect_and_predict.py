6df9dd41320426afa523dd7c94c53dea3a637ad5
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-11-11 09:50:33 +0100

    prediction des imotions

--- DL/detect_and_predict.py
+++ DL/detect_and_predict.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 model = tensorflow.keras.models.load_model("DL/emotion_cnn_model.keras")
 classes_names = ('angry', 'disguasted', 'fearful', 'happy', 'neutral', 'sad', 'surprised')
 facecascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-image = cv2.imread("C:/Users/hp/Pictures/Screenshots/im.JPG")
+image = cv2.imread("C:/Users/hp/Desktop/D-tection-d-motions-Faciales/new.JPG")
 gris = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 print(image.shape)
 
@@ -22,8 +22,22 @@ faces = facecascade.detectMultiScale(image, scaleFactor=1.1 , minNeighbors=5, mi
 for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
+face = image[y:y+h, x:x+w]
+face = image[y:y+h, x:x+w]
+face = cv2.resize(face, (48, 48))
+face = face.astype('float32') / 255.0
+face = tf.expand_dims(face, axis=-1)
+face = tf.expand_dims(face, axis=0)
+prediction = model.predict(face)
+emotion = classes_names[tf.argmax(prediction[0])]
+print(emotion)
 
 cv2.imwrite("new.JPG", image)
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+plt.imshow(image_rgb)
+plt.axis('off')
+plt.show()
+
 # plt.imshow(image, cmap='gray')
 # plt.show()
 
@@ -49,7 +63,7 @@ cv2.imwrite("new.JPG", image)
 # print("c'est tout bon")
 
 # cv2.imwrite("output_path", image)
-#     # visage = gris[y:y+h, x:x+w]
+#     # face = gris[y:y+h, x:x+w]
 #     # visage = cv2.resize(visage, (48,48))
 #     # visage = visage.astype('float32') / 255.0
 #     # visage = tf.expand_dims(visage, axis=-1)
