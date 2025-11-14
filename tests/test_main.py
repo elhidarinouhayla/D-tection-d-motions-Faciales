f31fd64c59b801b592653aa4fd04c8538027b3b9
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-11-14 16:08:20 +0100

    update

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,12 +3,12 @@ import pytest
 import sys
 import os
 from fastapi.testclient import TestClient
-from database import Base,engine
 import numpy as np
 
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from main import app
+from database import Base,engine
 from DL.detect_and_predict import emotion_detection
 @pytest.fixture(scope="module", autouse=True)
 def setup_database():
